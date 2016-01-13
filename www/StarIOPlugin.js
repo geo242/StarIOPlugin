0049d02bde264edfe0bdd2229c2597017730aedf
Geoff Tripoli <gtripoli@gmail.com>
2016-01-13 13:01:17 -0800

    Updated js to use module.exports

--- www/StarIOPlugin.js
+++ www/StarIOPlugin.js
@@ -1,56 +1,53 @@
-function StarIOPlugin() {}
+var exec = require("cordova/exec");
 
-StarIOPlugin.prototype.checkStatus = function (port, callback) {
-    cordova.exec(function(result) {
-            callback(null, result)
-        },
-        function(error) {
-            callback(error)
-        }, 'StarIOPlugin', 'checkStatus', [port]);
-}
-
-
-StarIOPlugin.prototype.portDiscovery = function (type, callback) {
-    type = type || 'All';
-    cordova.exec(function(result) {
-            callback(null, result)
-        },
-        function(error) {
-            callback(error)
-        }, 'StarIOPlugin', 'portDiscovery', [type]);
-}
-
-StarIOPlugin.prototype.printReceipt = function (port, receipt, callback) {
-    cordova.exec(function(result) {
-            callback(null, result)
-        },
-        function(error) {
-            callback(error)
-        }, 'StarIOPlugin', 'printReceipt', [port, receipt]);
-}
-
-StarIOPlugin.prototype.openCashDrawer = function (port, callback) {
-    cordova.exec(function(result) {
-            callback(null, result)
-        },
-        function(error) {
-            callback(error)
-        }, 'StarIOPlugin', 'openCashDrawer', [port]);
-}
-
-StarIOPlugin.prototype.connect = function (port, callback) {
-    var connected = false;
-    cordova.exec(function (result) {
-            //On initial connection - fire callback, otherwise fire a window event
-            if (!connected) {
-                callback(null, result);
-                connected = true;
-            } else {
-                //This event will be to notify of events like barcode scans
-                cordova.fireWindowEvent("starIOPluginData", result);
-            }
-        },
-        function (error) {
-            callback(error)
-        }, 'StarIOPlugin', 'connect', [port]);
-}
+module.exports = {
+    checkStatus: function (port, callback) {
+        exec(function (result) {
+                callback(null, result)
+            },
+            function (error) {
+                callback(error)
+            }, 'StarIOPlugin', 'checkStatus', [port]);
+    },
+    portDiscovery: function (type, callback) {
+        type = type || 'All';
+        exec(function (result) {
+                callback(null, result)
+            },
+            function (error) {
+                callback(error)
+            }, 'StarIOPlugin', 'portDiscovery', [type]);
+    },
+    printReceipt: function (port, receipt, callback) {
+        exec(function (result) {
+                callback(null, result)
+            },
+            function (error) {
+                callback(error)
+            }, 'StarIOPlugin', 'printReceipt', [port, receipt]);
+    },
+    openCashDrawer: function (port, callback) {
+        exec(function (result) {
+                callback(null, result)
+            },
+            function (error) {
+                callback(error)
+            }, 'StarIOPlugin', 'openCashDrawer', [port]);
+    },
+    connect: function (port, callback) {
+        var connected = false;
+        exec(function (result) {
+                //On initial connection - fire callback, otherwise fire a window event
+                if (!connected) {
+                    callback(null, result);
+                    connected = true;
+                } else {
+                    //This event will be to notify of events like barcode scans
+                    cordova.fireWindowEvent("starIOPluginData", result);
+                }
+            },
+            function (error) {
+                callback(error)
+            }, 'StarIOPlugin', 'connect', [port]);
+    }
+};
